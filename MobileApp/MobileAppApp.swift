86c5b99228a8eaac2b63016474bed4fccd62dfb5
RinesaBislimi <rinesa.bislimi@student.uni-pr.edu>
2024-02-15 17:20:58 -0800

    Initial Commit

---
+++ MobileApp/MobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAppApp.swift
+//  MobileApp
+//
+//  Created by Rinesa on 15.2.24.
+//
+
+import SwiftUI
+
+@main
+struct MobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
