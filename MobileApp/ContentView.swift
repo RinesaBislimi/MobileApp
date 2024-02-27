0c9e0094a1d3c5e23a052422b3cf4973a0c867fb
RinesaBislimi <rinesa.bislimi@student.uni-pr.edu>
2024-02-27 13:15:39 -0800

    Login icon

--- MobileApp/ContentView.swift
+++ MobileApp/ContentView.swift
@@ -13,14 +13,8 @@ struct ContentView: View {
             Home().tabItem{
                 Image(systemName:"house").font(.title)
             }
-            Text("activity").tabItem{
-                Image(systemName:"person.fill.checkmark").font(.title)
-            }
-            Text("search").tabItem{
-                Image(systemName:"magnifyingglass.circle").font(.title)
-            }
             Text("person").tabItem{
-                Image(systemName:"gear").font(.title)
+                Image(systemName:"person.fill").font(.title)
             }
         }
     }
